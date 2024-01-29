41cc28e5f269e864719370f34c17b72b3b81bc39
IntegralTeam <repo@sfxdx.com>
2024-01-29 14:33:38 +0700

    Change gasprice params

--- params/odyssey_params.go
+++ params/odyssey_params.go
@@ -13,8 +13,8 @@ import (
 const (
 	// MinGasPrice is the number of nDIONE required per gas unit for a
 	// transaction to be valid, measured in wei
-	LaunchMinGasPrice        int64 = 470_000_000_000
-	ApricotPhase1MinGasPrice int64 = 225_000_000_000
+	LaunchMinGasPrice        int64 = 2_380_952_380_952_381
+	ApricotPhase1MinGasPrice int64 = 2_380_952_380_952_381
 
 	OdysseyAtomicTxFee = 50 * units.Dione
 
@@ -22,12 +22,12 @@ const (
 	CortinaGasLimit       uint64 = 15_000_000
 
 	ApricotPhase3ExtraDataSize            uint64 = 80
-	ApricotPhase3MinBaseFee               int64  = 75_000_000_000
-	ApricotPhase3MaxBaseFee               int64  = 225_000_000_000
-	ApricotPhase3InitialBaseFee           int64  = 225_000_000_000
+	ApricotPhase3MinBaseFee               int64  = 2_380_952_380_952_381
+	ApricotPhase3MaxBaseFee               int64  = 7_142_857_142_857_143
+	ApricotPhase3InitialBaseFee           int64  = 2_380_952_380_952_381
 	ApricotPhase3TargetGas                uint64 = 10_000_000
-	ApricotPhase4MinBaseFee               int64  = 50_000_000_000
-	ApricotPhase4MaxBaseFee               int64  = 1_000_000_000_000
+	ApricotPhase4MinBaseFee               int64  = 2_380_952_380_952_381
+	ApricotPhase4MaxBaseFee               int64  = 7_142_857_142_857_143
 	ApricotPhase4BaseFeeChangeDenominator uint64 = 12
 	ApricotPhase5TargetGas                uint64 = 15_000_000
 	ApricotPhase5BaseFeeChangeDenominator uint64 = 36
