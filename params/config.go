8b20582877bf1ae056e152581eb017b446bc08c8
IntegralTeam <repo@sfxdx.com>
2025-08-04 12:50:17 +0700

    Update hardfork time

--- params/config.go
+++ params/config.go
@@ -109,7 +109,7 @@ var (
 		CortinaBlockTimestamp:           utils.NewUint64(0),
 		DurangoBlockTimestamp:           utils.NewUint64(0),
 		CancunTime:                      utils.NewUint64(1747040400),
-		ApricotPhase7BlockTimestamp:     utils.NewUint64(1754046000),
+		ApricotPhase7BlockTimestamp:     utils.NewUint64(1754294400),
 	}
 
 	// OdysseyLocalChainConfig is the configuration for the Odyssey Local Network
